3306d9a0a421d8988a3663114038bbd2839a18de
Tobydrama <Tobe-madsen@hotmail.no>
2021-04-08 16:59:54 +0200

    added comments

--- FMI.py
+++ FMI.py
@@ -6,12 +6,15 @@ import ast
 
 
 def main():
-    print('\033[92m' + "> Welcome to the FMI client!" + '\033[0m')
-    print("> You can choose either a"
-          + '\033[94m' + " command line interface" + '\033[0m'
-          + " mode, or a "
-          + '\033[94m' + "web app mode" + '\033[0m')
-    print ("> CLI shows data in this console, while web app mode is a web app that shows data as graphs on a website.")
+    """
+    Entry point for application
+    Writes a prompt to the console which asks user which mode they wish to start the application
+    Starts storage and weather station server in CLI mode
+
+    :rtype: None
+    """
+
+    print_console_information()
     mode = input("> Type "
                  + '\033[93m' + "'cli' " + '\033[0m'
                  + "or "
@@ -23,15 +26,8 @@ def main():
 
     elif mode == "cli":
         print("Starting storage and weather services...")
-        ws = threading.Thread(target=run_weather_station)
-        s = threading.Thread(target=run_storage)
-        ws.start()
-        s.start()
-        sleep(2)
-
-        sock = socket()
-        server_address = ("localhost", 5556)
-        sock.connect(server_address)
+        start_station_and_storage_threads()
+        sock = init_socket_connection()
 
         while (True):
             sentence = input("> type: give last or give all:\n")
@@ -76,11 +72,67 @@ def print_all_data_from_storage(rawData):
         num += 1
 
 
+def start_station_and_storage_threads():
+    """
+    Starts the storage and the weather station servers in other threads
+
+    :rtype: None
+    """
+
+    ws = threading.Thread(target=run_weather_station)
+    s = threading.Thread(target=run_storage)
+    ws.start()
+    s.start()
+
+    # Gives the storage thread and weather station threads time initialize
+    sleep(2)
+
+
+def print_console_information():
+    """
+    Prints information about what interface the users wants to use
+
+    :rtype: None
+    """
+
+    print('\033[92m' + "> Welcome to the FMI client!" + '\033[0m')
+    print("> You can choose either a"
+          + '\033[94m' + " command line interface" + '\033[0m'
+          + " mode, or a "
+          + '\033[94m' + "web app mode" + '\033[0m')
+    print("> CLI shows data in this console, while web app mode is a web app that shows data as graphs on a website.")
+
+
+def init_socket_connection():
+    """
+    Starts the TCP socket and connects it to the server
+
+    :rtype: socket
+    """
+
+    sock = socket()
+    server_address = ("localhost", 5556)
+    sock.connect(server_address)
+    return sock
+
+
 def run_weather_station():
+    """
+    Run weather station server
+
+    :rtype: None
+    """
+
     os.system("python code/weather-station/weather-station-server.py")
 
 
 def run_storage():
+    """
+    Run storage server
+
+    :rtype: None
+    """
+
     os.system("python code/storage/storage.py")
 
 
